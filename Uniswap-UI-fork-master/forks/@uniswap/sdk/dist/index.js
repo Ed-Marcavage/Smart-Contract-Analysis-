e45a4caaf3af0d3229a4e21686c81d9a5108be88
Ed-Marcavage <edmarcavage256@proton.me>
2022-11-06 18:55:35 -0500

    Break down by type

---
+++ Uniswap-UI-fork-master/forks/@uniswap/sdk/dist/index.js
@@ -0,0 +1,7 @@
+'use strict'
+
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./sdk.cjs.production.min.js')
+} else {
+  module.exports = require('./sdk.cjs.development.js')
+}
--- scripts/analysis.py
+++ scripts/analysis.py
@@ -5,4 +5,29 @@ def main():
 
 def Analysis(address):
     contract = Contract.from_explorer(address)
-    print(contract.info())
+    abi = contract.abi
+    d = {}
+    for value in abi:
+        if value['type'] in d:
+            d[value['type']] += 1
+        else:
+            d[value['type']] = 1
+    for key in list(d.keys()):
+        print(key, ":", d[key])
+
+# contract = Contract.from_explorer(address)
+#     abi = contract.abi
+#     inputTypes = {}
+#     for value in abi:
+#         for input in value['inputs']:
+#             if input['type'] in inputTypes:
+#                 inputTypes[input['type']] += 1
+#             else:
+#                 inputTypes[input['type']] = 1
+#     for key in list(inputTypes.keys()):
+#         print(key, ":", inputTypes[key])
+
+
+
+    
+    
